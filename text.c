c321886e18c232e2b8c2527b3341cd4162f10562
jd <jean.daniel.michaud@gmail.com>
2020-02-03 18:05:56 +0100

    Add text source

---
+++ text.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdarg.h>
+#include <string.h>
+
+#include "raylib.h"
+
+#include "text.h"
+
+/**
+ * Load an image as a font. Expect each character to be 5x7 pixels and separated
+ * each by 1 pixel.
+ * Example: dash charater:
+ * 010100
+ * 010100
+ * 111110
+ * 010100
+ * 111110
+ * 010100
+ * 010100
+ */
+Texture2D loadFont(char *filename) {
+  const Image image = LoadImage(filename);
+  return LoadTextureFromImage(image);
+}
+
+/**
+ * Print a string of characters of font at position posX, posY
+ */
+void print(const char *s, const uint16_t posX, const uint16_t posY,
+  const Texture2D *font, const Color color) {
+  for (uint16_t i = 0; s[i] != 0; ++i) {
+    const char c = s[i];
+    if (c > 32 && c < 126) {
+      DrawTextureRec(
+        *font,
+        (Rectangle) { (c - 33) * 6, 0, 5, 7 },
+        (Vector2) { posX + i * 6, posY },
+        color
+      );
+    }
+  }
+}
+
+void vprint(const uint16_t posX, const uint16_t posY, const Texture2D *font,
+  const Color color, char *format, ...) {
+  va_list args;
+  static char s[256];
+  memset(s, 0, 256 * sizeof (char));
+  va_start(args, format);
+  snprintf(s, 256, format, args);
+  va_end(args);
+  print(s, posX, posY, font, color);
+}
---
+++ text.h
@@ -0,0 +1,12 @@
+#ifndef __TEXT_H__
+#define __TEXT_H__
+
+#include "raylib.h"
+
+Texture2D loadFont(char *filename);
+void print(const char *s, const uint16_t posX, const uint16_t posY,
+  const Texture2D *font, const Color color);
+void vprint(const uint16_t posX, const uint16_t posY, const Texture2D *font,
+  const Color color, char *format, ...);
+
+#endif // !__TEXT_H__
